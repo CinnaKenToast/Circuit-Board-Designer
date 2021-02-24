c9dcb329716c2e3a91242eb766bc3bd4c483ead0
Ken Shipley <rockmankim@gmail.com>
2021-02-24 09:33:45 -0600

    Updated classes

--- src/classes.py
+++ src/classes.py
@@ -19,7 +19,7 @@ class Component:
         self.p_orirent = [int][int]
         self.connections: {int:int}
         self.path = [int][int][int]
-        self.schematic = schematic
+        self.schematic = Schematic
     
     def addLabel(self, label):
          raise NotImplementedError("Abstract method")
@@ -53,12 +53,14 @@ class Schematic:
     def addWire(self, component1, component2):
         index1 = self.components.index(component1)
         index2 = self.components.index(component2)
+        # UNSURE HOW TO IMPLEMENT CONNECTIONS
+
 
     def snipWire(self, component1, component2):
         pass
 
-    def addComponent(self, component):
-        self.components.append(component)
+    def addComponent(self, id, s_pos, s_orient):
+        self.components.append(Component(id, s_pos, s_orient))
 
     def deleteComponent(self, component):
         self.components.remove(component)
