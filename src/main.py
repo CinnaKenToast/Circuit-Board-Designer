251095208687a347878530046930118ae6c29326
CinnaKenToast <rockmankim@gmail.com>
2021-05-04 04:20:33 -0500

    Added for file functionality

--- src/main.py
+++ src/main.py
@@ -138,6 +138,8 @@ class MainWindow(QMainWindow):
         self.ids = 2
         self.penColor = QtGui.QPen(QtCore.Qt.black)
         self.penColor.setWidth(3)
+        self.outlineColor = QtGui.QPen(QtCore.Qt.black)
+        self.outlineColor.setWidth(3)
         self.file = None
 
         self.components = []
@@ -145,14 +147,14 @@ class MainWindow(QMainWindow):
         self.connections = []
 
 # ------------------ Test Components ------------------
-        self.component1 = Component(self.scene, self.penColor, "Resistor", "r1", 0)
+        self.component1 = Component(self.scene, self.outlineColor, "Resistor", "r1", 0)
         self.component1.boundingBox.moveBy(1000, 1000)
         self.component1.schematicArgs = {"id": 0, "label": self.component1.name, "component_type": "Resistor"} 
         self.schematic.add_component(self.component1.schematicArgs)
         self.schematic.set_component_schematic_pos(0, [self.component1.boundingBox.pos().x(), self.component1.boundingBox.pos().y()])
         self.components.append(self.component1)
 
-        self.component2 = Component(self.scene, self.penColor, "Diode", "d1", 1)
+        self.component2 = Component(self.scene, self.outlineColor, "Diode", "d1", 1)
         self.component2.boundingBox.moveBy(1500, 1000)
         self.component2.schematicArgs = {"id": 1, "label": self.component2.name, "component_type": "Diode"} 
         self.schematic.add_component(self.component2.schematicArgs)
@@ -174,13 +176,13 @@ class MainWindow(QMainWindow):
         }
 
         # Connect buttons to functions
-        self.ui.btn_resistor.clicked.connect(lambda: self.addComponent("Resistor", "r1"))
-        self.ui.btn_capacitor.clicked.connect(lambda: self.addComponent("Capacitor", "c1"))
-        self.ui.btn_diode.clicked.connect(lambda: self.addComponent("Diode", "d1"))
-        self.ui.btn_led.clicked.connect(lambda: self.addComponent("Led", "l1"))
-        self.ui.btn_inductor.clicked.connect(lambda: self.addComponent("Inductor", "i1"))
-        self.ui.btn_switch.clicked.connect(lambda: self.addComponent("Switch", "s1"))
-        self.ui.btn_voltage.clicked.connect(lambda: self.addComponent("VoltageSource", "v1"))
+        self.ui.btn_resistor.clicked.connect(lambda: self.addComponent("Resistor"))
+        self.ui.btn_capacitor.clicked.connect(lambda: self.addComponent("Capacitor"))
+        self.ui.btn_diode.clicked.connect(lambda: self.addComponent("Diode"))
+        self.ui.btn_led.clicked.connect(lambda: self.addComponent("Led"))
+        self.ui.btn_inductor.clicked.connect(lambda: self.addComponent("Inductor"))
+        self.ui.btn_switch.clicked.connect(lambda: self.addComponent("Switch"))
+        self.ui.btn_voltage.clicked.connect(lambda: self.addComponent("VoltageSource"))
 
         self.ui.btn_delete.clicked.connect(lambda: self.deleteComponent())
 
@@ -190,12 +192,32 @@ class MainWindow(QMainWindow):
         self.ui.btn_design.clicked.connect(lambda: self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_design))
         self.ui.btn_convert.clicked.connect(lambda: self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_convert))
         self.ui.btn_file.clicked.connect(lambda: self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_file))
+        self.ui.btn_design.clicked.connect(lambda: self.toggleMenu(200, True))
+        self.ui.btn_convert.clicked.connect(lambda: self.toggleMenu(200, True))
+        self.ui.btn_file.clicked.connect(lambda: self.toggleMenu(200, True))
 
         self.ui.btn_wire.clicked.connect(lambda: self.toggleTools(50, "btn_wire"))
         self.ui.btn_snip.clicked.connect(lambda: self.toggleTools(50, "btn_snip"))
         self.ui.btn_delete.clicked.connect(lambda: self.toggleTools(50, "btn_delete"))
         self.ui.btn_label.clicked.connect(lambda: self.toggleTools(50, "btn_clicked"))
         self.ui.btn_comment.clicked.connect(lambda: self.toggleTools(50, "btn_comment"))
+        self.ui.btn_resistor.clicked.connect(lambda: self.toggleTools(50, "btn_resistor"))
+        self.ui.btn_capacitor.clicked.connect(lambda: self.toggleTools(50, "btn_capacitor"))
+        self.ui.btn_diode.clicked.connect(lambda: self.toggleTools(50, "btn_diode"))
+        self.ui.btn_led.clicked.connect(lambda: self.toggleTools(50, "btn_led"))
+        self.ui.btn_inductor.clicked.connect(lambda: self.toggleTools(50, "btn_inductor"))
+        self.ui.btn_switch.clicked.connect(lambda: self.toggleTools(50, "btn_switch"))
+        self.ui.btn_voltage.clicked.connect(lambda: self.toggleTools(50, "btn_voltage"))
+        self.ui.btn_black.clicked.connect(lambda: self.toggleTools(50, "btn_black"))
+        self.ui.btn_red.clicked.connect(lambda: self.toggleTools(50, "btn_red"))
+        self.ui.btn_orange.clicked.connect(lambda: self.toggleTools(50, "btn_orange"))
+        self.ui.btn_yellow.clicked.connect(lambda: self.toggleTools(50, "btn_yellow"))
+        self.ui.btn_green.clicked.connect(lambda: self.toggleTools(50, "btn_green"))
+        self.ui.btn_blue.clicked.connect(lambda: self.toggleTools(50, "btn_blue"))
+        self.ui.btn_purple.clicked.connect(lambda: self.toggleTools(50, "btn_purple"))
+        self.ui.btn_pink.clicked.connect(lambda: self.toggleTools(50, "btn_pink"))
+        self.ui.btn_cyan.clicked.connect(lambda: self.toggleTools(50, "btn_cyan"))
+        self.ui.btn_brown.clicked.connect(lambda: self.toggleTools(50, "btn_brown"))
 
         self.ui.btn_add.clicked.connect(lambda: self.ui.stacked_tools.setCurrentWidget(self.ui.page_components))
         self.ui.btn_color.clicked.connect(lambda: self.ui.stacked_tools.setCurrentWidget(self.ui.page_colors))
@@ -247,14 +269,17 @@ class MainWindow(QMainWindow):
     def printSchematic(self):
         for component in self.schematic.components.values():
             print(component.to_string())
-        print(self.connections)
+        print("connections:", self.connections)
+        print("components:", self.components)
 
     # Add component to the scene
-    def addComponent(self, component, name):
-        name, ok = QtWidgets.QInputDialog.getText(self, 'Component Name', 'Enter component name:')
+    def addComponent(self, component, name = '', posX = 1500, posY = 1000):
+        ok = True
+        if name == '':
+            name, ok = QtWidgets.QInputDialog.getText(self, 'Component Name', 'Enter component name:')
         if ok:
-            newComponent = Component(self.scene, self.penColor, component, name, self.ids)
-            newComponent.boundingBox.moveBy(1500,1000)
+            newComponent = Component(self.scene, self.outlineColor, component, name, self.ids)
+            newComponent.boundingBox.moveBy(posX,posY)
             newComponent.schematicArgs = {"id": self.ids, "label": newComponent.name, "component_type": component} 
             newComponent.id = self.ids
             self.schematic.add_component(newComponent.schematicArgs)
@@ -278,29 +303,61 @@ class MainWindow(QMainWindow):
                 del comp
 
     # Adds connection between two components
-    def addConnection(self):
-        if len(eventList) < 2:
-            print("Not enough events")
-        elif eventList[0][0] == eventList[1][0]:
-            print("Can't connect the pins of same component")
-        else:
-            line = self.scene.addLine(QtCore.QLineF(eventList[0][1], eventList[1][1]), self.penColor)
-            pinId0 = str(eventList[0][2][0]) + "_" + str(eventList[0][2][1]) 
-            pinId1 = str(eventList[1][2][0]) + "_" + str(eventList[1][2][1]) 
-            self.connections.append(line)
-            self.schematic.add_connection(pinId0, pinId1)
+    def addConnection(self, comp0Id = None, comp1Id = None):
+        if comp0Id == None and comp1Id == None:
+            if len(eventList) < 2:
+                print("Not enough events")
+            elif eventList[0][0] == eventList[1][0]:
+                print("Can't connect the pins of same component")
+            else:
+                line = self.scene.addLine(QtCore.QLineF(eventList[0][1], eventList[1][1]), self.penColor)
+                pinId0 = str(eventList[0][2][0]) + "_" + str(eventList[0][2][1]) 
+                pinId1 = str(eventList[1][2][0]) + "_" + str(eventList[1][2][1]) 
+                self.connections.append(line)
+                self.schematic.add_connection(pinId0, pinId1)
+                for component in self.components:
+                    if component.id == eventList[0][2][0]: # Check if component is first event 
+                        if eventList[0][2][1] == 0: # which pin was pressed
+                            component.pin0Connection = line
+                        else:
+                            component.pin1Connection = line
+                    elif component.id == eventList[1][2][0]: # Check if component is second event
+                        if eventList[1][2][1] == 0: # Which pin was pressed
+                            component.pin0Connection = line
+                        else:
+                            component.pin1Connection = line
+        else: # This is only run when the program is loading in a file
+            comp0Id, comp0Btn = int(comp0Id.split("_"))
+            comp1Id, comp1Btn = int(comp1Id.split("_"))
             for component in self.components:
-                if component.id == eventList[0][2][0]: # Check if component is first event 
-                    if eventList[0][2][1] == 0: # which pin was pressed
+                if component.id == comp0Id:
+                    if comp0Btn == 0:  
+                        pin0x = component.boundingBox.pos().x() + 12
+                        pin0y = component.boundingBox.pos().y() + 25 + 60/2
+                    else:
+                        pin0x = component.boundingBox.pos().x() + 170 - 12
+                        pin0y = component.boundingBox.pos().y() + 25 + 60/2
+                elif component.id == comp1Id:
+                    if comp0Btn == 0:  
+                        pin1x = component.boundingBox.pos().x() + 12
+                        pin1y = component.boundingBox.pos().y() + 25 + 60/2
+                    else:
+                        pin1x = component.boundingBox.pos().x() + 170 - 12
+                        pin1y = component.boundingBox.pos().y() + 25 + 60/2
+            line = self.scene.addLine(QtCore.QLineF(pin0x, pin0y, pin1x, pin1y, self.penColor))
+            self.connections.append(line)
+            for component in self.components:
+                if component.id == comp0Id:
+                    if comp0Btn == 0:
                         component.pin0Connection = line
                     else:
                         component.pin1Connection = line
-                elif component.id == eventList[1][2][0]: # Check if component is second event
-                    if eventList[1][2][1] == 0: # Which pin was pressed
+                elif component.id == comp1Id:
+                    if comp1Btn == 0:
                         component.pin0Connection = line
                     else:
                         component.pin1Connection = line
-            print(self.connections)
+        print(self.connections)
     
     # Delete the connection between two components
     def removeConnection(self):
@@ -380,14 +437,42 @@ class MainWindow(QMainWindow):
         print(color)
         self.toggleTools(50, "btn_pick_color")
 
+    def clearSchematic(self):
+        for component in self.components:
+            compId = component.id
+            self.schematic.remove_component(compId)
+            del component
+        self.components.clear()
+        self.connections.clear()
+        self.scene.clear()
+        self.file = None
+        self.ids = 0
 
     # Opens a file
     def fileOpen(self):
-        pass
+        self.clearSchematic()
+        fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '', 'Circuit Data (*.circ)')
+        self.schematic.load(fileName[0])
+        for component in self.schematic.components.values():
+            self.addComponent(component.__class__.__name__, component.label, component.schem_position[0], component.schem_position[1])
+            if component.id > self.ids:
+                self.ids = component.id
+        #for connection in self.schematic.connections_list:
+            #print("HERE MOTHER FUCKER")
+            #self.addConnection(connection[0], connection[1])
+
+        #self.printSchematic()
+        self.file = fileName[0]
+        self.ui.label_file_location.setText(self.file)
+        self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_design)
+
 
     # Creates a new file
     def fileCreate(self):
-        pass
+        self.clearSchematic()
+        self.ui.label_file_location.setText("")
+        self.ui.window_canvas.centerOn(1500.0, 1000.0)
+        self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_design)
 
     # Saves a file
     def fileSave(self):
@@ -395,16 +480,17 @@ class MainWindow(QMainWindow):
             self.fileSaveAs()
         else:
             self.schematic.overwrite_save(self.file)     
+            self.ui.window_canvas.centerOn(1500.0, 1000.0)
             self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_design)
 
     # Saves as a new file
     def fileSaveAs(self):
-        fileName = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File','', 'Circuit (*.circ)')
+        fileName = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File','', 'Circuit Data (*.circ)')
         print(fileName[0])
         self.schematic.overwrite_save(fileName[0])
         self.file = fileName[0]
         self.ui.label_file_location.setText(self.file)
-        self.toggleMenu(200, True)
+        self.ui.window_canvas.centerOn(1500.0, 1000.0)
         self.ui.stacked_workspaces.setCurrentWidget(self.ui.page_design)
 
     # Toggles the left pull out menu for the different screens
@@ -430,7 +516,9 @@ class MainWindow(QMainWindow):
     # Toggles the right pull out menu for the tools
     def toggleTools(self, maxWidth, button):
         enable = False
-        buttons = ["btn_wire", "btn_snip", "btn_delete", "btn_clicked", "btn_comment", "btn_pick_color"]
+        buttons = ["btn_wire", "btn_snip", "btn_delete", "btn_clicked", "btn_comment", "btn_pick_color", "btn_resistor", 
+                "btn_capacitor", "btn_diode", "btn_led", "btn_inductor", "btn_switch", "btn_voltage", "btn_black", "btn_red", 
+                "btn_orange", "btn_yellow", "btn_green", "btn_blue", "btn_purple", "btn_pink", "btn_cyan", "btn_brown"]
         
         # print("Start:", button, "|", self.currentState)
 
