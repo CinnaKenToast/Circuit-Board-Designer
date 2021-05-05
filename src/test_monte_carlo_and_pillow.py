97126413dd09749cd45096a7c899de70f5c31204
Amuenster <42555398+Amuenster@users.noreply.github.com>
2021-05-04 22:20:38 -0500

    Testing

--- src/test_monte_carlo_and_pillow.py
+++ src/test_monte_carlo_and_pillow.py
@@ -10,18 +10,19 @@ def main():
     sch = classes.Schematic()
     # Use gui generated schematic:
     sch.load("jason_test_monte_through_gui.circ")
+    sch.print_all_components_strings()
 
     do_monte_carlo(sch)
     convert_to_pcb_image(sch, "test_layout_conversion.png")
-    # sch.update_all_pin_positions_to_trimmed_layout()
+    sch.get_offset_for_label()
 
     sch.overwrite_save("test_conversion.json")
 
 
 def do_monte_carlo(sch):
     # Monte_carlo w/ output:
-    sch.set_monte_carlo_parameters(3, 0, .99)
-    sch.monte_carlo(300)
+    sch.set_monte_carlo_parameters(4, 3, .9)
+    sch.monte_carlo(50)
 
 
 def convert_to_pcb_image(sch, file_name):
